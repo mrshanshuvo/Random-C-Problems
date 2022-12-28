35c57ed8cc34364594bc7b3a80bd8e7143d1c114
Shan Shuvo <rtsshuvo6434@gmail.com>
2022-12-29 01:05:13 +0600

    Sorting

--- bubbleSort.c
+++ bubbleSort.c
@@ -10,7 +10,7 @@ struct sort
 void main()
 {
     struct sort find;
-    int i;
+    int i,j,temp;
     printf("Enter how many elements you wish to add : ");
     scanf("%d", &find.num);
 
@@ -18,5 +18,31 @@ void main()
     for(i=0; i<find.num; i++)
     {
         scanf("%d", &find.data[i]);
+    }
+
+    printf("\nThe given data are : \n");
+    for(i=0; i<find.num; i++)
+    {
+        printf("%d\t",find.data[i]);
+    }
+    //bubble sorting
+    for(i=0; i<find.num-1; i++)
+    {
+        for(j=0; j<find.num-i-1; j++)
+        {
+            if(find.data[j]>find.data[j+1])
+            {
+                temp = find.data[j];
+                find.data[j] = find.data[j+1];
+                find.data[j+1] = temp;
+            }
+
+        }
+    }
+    //showing the sorted data
+    printf("\n\nThe Sorted Data are : \n\n");
+    for(i=0; i<find.num; i++)
+    {
+        printf("%d\t",find.data[i]);
     }
 }
